8cc3a40224ca524245a8952d6a9ed2d65c716e73
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-02 06:04:00 +0000

    if-else statements

---
+++ Jenny_lectures/28-if_else/1-if_else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age;
+
+	printf("Enter your age:\n");
+	scanf("%d", &age);
+
+	if (age > 25 && age < 30)
+	{
+		printf("Your age is %d.\n", age);
+		printf("You are eligible\n");
+	}
+	else
+	{
+		printf("Your age is %d.\n", age);
+		printf("You are not eligible.\n");
+	}
+	printf("Thank you for your time!\n");
+}
