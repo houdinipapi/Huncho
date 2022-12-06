1c239a03cafc3099c222587c4dfcbcbe00492e93
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-06 13:35:24 +0000

    numbers

---
+++ Jenny_lectures/for_loop/3-for_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - function that prints all numbers between 1 - 100
+  */
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		printf("%d ", i);
+	}
+}
