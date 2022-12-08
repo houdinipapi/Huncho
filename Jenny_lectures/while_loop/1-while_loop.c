58a1ae0b617ebf90556d303d9a3154562351227a
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-08 17:49:07 +0000

    Commit

---
+++ Jenny_lectures/while_loop/1-while_loop.c
@@ -0,0 +1,15 @@
+/* prints 1 to 10 using while loop */
+
+#include <stdio.h>
+
+void main(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	printf("End of program\n");
+}
