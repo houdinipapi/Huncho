0a495b4350d8e04f774bb411792a4b01a515b6de
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-19 19:00:27 +0000

    print _putchar

---
+++ alx_practicals/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - a program that prints _putchar
+  * followed by a new line
+  * the program should return 0
+  * Return: 0
+  */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
