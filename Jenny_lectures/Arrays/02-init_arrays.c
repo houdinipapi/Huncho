571b78c57bda2cb36da832894a8b4974c8156f0d
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-25 04:30:58 +0000

    printing arrays

---
+++ Jenny_lectures/Arrays/02-init_arrays.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main -initializing and printing arrays
+  */
+
+void main(void)
+{
+	int a[5], i;
+
+	printf("Enter an integer:\n");
+
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+
+	printf("The rray integers are:\n");
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+}
