545343a7d6786a953edfe9899df423cfb3282b7d
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-26 10:47:20 +0000

    even and odd numbers

--- Jenny_lectures/Arrays/07-arrays.c
+++ Jenny_lectures/Arrays/07-arrays.c
@@ -15,13 +15,13 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		scanf("%d", &a[i]);
-	}
-	for (i = 0; i < 10; i++)
+	/* for (i = 0; i < 10; i++) */
 	{
 		if (a[i] % 2 == 0)
 			even++;
 		else
 			odd++;
+	}
 	}
 	printf("The even numbers are %d\n", even);
 	printf("The odd numbers are %d\n", odd);
