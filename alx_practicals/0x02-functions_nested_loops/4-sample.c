d8495dd98952557e3a09adb67852039f82f70927
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-26 10:50:14 +0000

    Not functioning properly

---
+++ alx_practicals/0x02-functions_nested_loops/4-sample.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - checks the code
+  */
+
+void main(void)
+{
+	int i, r;
+
+	printf("Enter a character:\n");
+	scanf("%d", &i);
+
+	r = _isalpha(i);
+}
