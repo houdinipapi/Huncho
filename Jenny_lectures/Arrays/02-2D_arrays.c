dfa3ae7a439ffcfc7f48cd09588f3930f3d49d0e
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-27 18:56:22 +0000

    Arrays

---
+++ Jenny_lectures/Arrays/02-2D_arrays.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - prints matrices
+  */
+
+void main(void)
+{
+	int arr[3][3];
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("Enter arr[%d][%d]:", i, j);
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	printf("\n Printing the elements in a matrix...\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d\t", arr[i][j]);
+			if (j == 3)
+			{
+				printf("\n");
+			}
+		}
+	}
+}
