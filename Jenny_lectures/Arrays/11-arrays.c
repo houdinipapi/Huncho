529694b9530a160ac5d6026cb040018b179f65e8
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-28 21:00:17 +0000

    printing remaining letters

--- Jenny_lectures/Arrays/11-arrays.c
+++ Jenny_lectures/Arrays/11-arrays.c
@@ -1,1 +1,25 @@
-#include <stdio
+#include <stdio.h>
+
+/**
+  * main - a array that prints alphabets
+  */
+
+void main(void)
+{
+	char n, i;
+
+	printf("Enter an alphabet:\n");
+	scanf("%c", &n);
+
+	for (i = n; i <= 'z'; i++)
+	{
+		printf("%c ", i);
+		if (n == 'z')
+		{
+			for (i = n; i >= 'a'; i--)
+			{
+				printf("%c ", i);
+			}
+		}
+	}
+}
