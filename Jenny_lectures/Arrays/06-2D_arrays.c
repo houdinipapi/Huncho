dae8045585fa589859e4b08a89334861f4969889
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-29 18:28:37 +0000

    Trying to print arrays

---
+++ Jenny_lectures/Arrays/06-2D_arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - prints the subscript of arrays
+  */
+
+void main(void)
+{
+	int arr[3][3] = {{4, 5, 6}, {4, 5, 6}, {4, 5, 6}};
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("arr[%d][%d]\n", i, j);
+			printf("%d\t %d", arr[i][j]);
+			if (j == 2)
+				putchar('\n');
+		}
+	}
+}
