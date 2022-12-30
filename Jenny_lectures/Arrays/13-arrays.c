21cd275e2d71cfd1713042ec9f71bec4fce7b0af
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-30 13:26:46 +0000

    Address of arrays

---
+++ Jenny_lectures/Arrays/13-arrays.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - prints array address
+  */
+
+void main(void)
+{
+	int arr[4];
+	int i;
+
+	printf("Inserting data elements.\n");
+
+	for (i = 0; i < 4; i++)
+	{
+		printf("arr[%d]: \n", i);
+		scanf("%d", &arr[i]);
+	}
+
+	putchar('\n');
+	for (i = 0; i < 4; i++)
+	{
+		printf("%d\t %p\n", arr[i], arr[i]);
+	}
+}
