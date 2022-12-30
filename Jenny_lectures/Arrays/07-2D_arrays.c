9cced51cf54468a6cd36df8e3662057654787f98
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-30 18:10:13 +0000

    print array elements and sum

---
+++ Jenny_lectures/Arrays/07-2D_arrays.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - prints elements of 2D arrays
+  * and the sum
+  */
+
+void main(void)
+{
+	int arr[2][3];
+	int i, j;
+	int sum = 0;
+
+	printf("Enter the elements of 2D arrays:\n");
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("arr[%d][%d]:\n", i, j);
+			scanf("%d", &arr[i][j]);
+		}
+	}
+
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d\t", arr[i][j]);
+			sum = sum + arr[i][j];
+		}
+		printf("\n");
+	}
+	printf("%d\n", sum);
+}
