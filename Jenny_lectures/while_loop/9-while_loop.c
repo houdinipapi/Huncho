d3c74d4e124aad5629f3ac6dccbc056e72caaca3
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-31 19:03:44 +0000

    print pattern

---
+++ Jenny_lectures/while_loop/9-while_loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - prints a pattern
+  */
+
+void main(void)
+{
+	int i = 0, j;
+
+	printf("Printing a pattern\n");
+
+	while (i < 7)
+	{
+		j = 1;
+		while (j < 7)
+		{
+			printf("%d ", j);
+			j++;
+		}
+		i++;
+	}
+}
