71be239082ece0bcc25a15526cf243e19330532b
houdinipapi <ochiengcliff.co@gmail.com>
2022-12-31 19:10:54 +0000

    inverted pattern

---
+++ Jenny_lectures/while_loop/10-while_loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - prints a pattern
+  */
+
+void main(void)
+{
+	int i = 1, j;
+
+	printf("Printing a pattern\n");
+
+	while (i < 7)
+	{
+		j = 7;
+		while (j <= i)
+		{
+			printf("%d ", j);
+			j--;
+		}
+		putchar('\n');
+		i++;
+	}
+}
