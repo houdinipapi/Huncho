d87dde983502745b0ae74d32ec4d52f8e82d3f9b
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-09 15:23:18 +0000

    Reading and printing strings

---
+++ Jenny_lectures/Strings_in_C/string01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - reading and printing a string
+ */
+
+void main(void)
+{
+	char papi[10];
+
+	printf("Enter a string:");
+	scanf("%s", papi);
+	printf("The string you entered is %s\n", papi);
+}
