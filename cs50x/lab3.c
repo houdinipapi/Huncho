40029c41307bf0ee1d77fb9d2df908546621ddee
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-13 21:38:54 +0000

    Population and years

--- cs50x/lab3.c
+++ cs50x/lab3.c
@@ -13,19 +13,19 @@ int main(void)
 	do {
 		printf("Enter the current population: ");
 		scanf("%d", &p);
-	} while (p <= 9);
+	} while (p < 9);
 
 	do {
 		printf("Enter the target population: ");
 		scanf("%d", &tp);
-	} while (tp <= p);
+	} while (tp < p);
 
 	for (y = 0; p < tp; y++)
 	{
 		p += (p / 3) - (p / 4);
 	}
 
-	printf("Number of years = %d", y);
+	printf("Number of years = %d\n", y);
 
 	return (0);
 }
