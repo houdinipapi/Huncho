096c6e5312ae35a5f214977c54007635c05a8bd0
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-15 14:01:50 +0000

    Right-aligned staircase

---
+++ cs50x/pattern03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints a right-aligned pyramid
+ */
+
+void main(void)
+{
+	int i, j, n;
+
+	/* Asks for no. of rows */
+	printf("Enter the no. of rows: \n");
+	scanf("%d", &n);
+
+	/* printing the output */
+	for (i = 1; i <= n; i++)
+	{
+		/* printing spaces */
+		for (j = 1; j <= (n - i); j++)
+		{
+			printf(" ");
+		}
+
+		/* printing pattern */
+		for (j = 1; j <= i; j++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
