fabb160216da8ac9f016d8b9dbd59094354390ee
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-16 20:01:24 +0000

    initial commit

---
+++ houdinipapi/Sams-teach_yourself/mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints a mario pattern
+ */
+
+void main(void)
+{
+	int n, i, j, k, l;
+
+	k = l = 8;
+
+	do
+	{
+		printf("Enter a number of rows: \n");
+		scanf("%d", &n);
+	}
+	while (n < 1 || n > 9);
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("%8d %-8d\n", k, l);
+		}
+		//printf("\n");
+	}
+}
