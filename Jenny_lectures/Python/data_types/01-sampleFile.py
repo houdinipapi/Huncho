367e4b1136a24b0626253cac3f8ac3469f70083e
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-21 05:34:19 +0000

    data types

---
+++ Jenny_lectures/Python/data_types/01-sampleFile.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+name = input("What is your name? ")
+age = input("How old are you? ")
+y_o_b = 2022 - int(age)
+print("Hello " + name + ", " + "your year of birth is " + str(y_o_b))
