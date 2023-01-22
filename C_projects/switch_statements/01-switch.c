6e06b7cdf1d7223f64f2c93e5cb1439e74748c3b
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-22 19:32:41 +0000

    switch statement

---
+++ C_projects/switch_statements/01-switch.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - functioning of switch statements
+ * Return: 0
+ */
+
+int main(void)
+{
+	char grade;
+
+	printf("Enter your grade: ");
+	scanf(" %c", &grade);
+
+	switch (grade)
+	{
+		case 'A':
+			{
+				printf("Excelleent\n");
+			}
+			break;
+		case 'B':
+			{
+				printf("Good\n");
+			}
+			break;
+		case 'C':
+			{
+				printf("Average\n");
+			}
+			break;
+		case 'D':
+			{
+				printf("Poor\n");
+			}
+			break;
+		case 'E':
+			{
+				printf("Fail\n");
+			}
+			break;
+
+		default:
+			printf("Invalid Grade\n");
+	}
+
+	return (0);
+}
