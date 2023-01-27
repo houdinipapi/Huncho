fa09e6d6f30e92bb9301af89eba8535337ba8f13
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-27 14:11:31 +0000

    tip calculator

--- clever_python/tip_calculator.py
+++ clever_python/tip_calculator.py
@@ -4,4 +4,7 @@ food_amount = float(input("Enter your food amount: "))
 tip_percentage = float(input("Enter your tip %: ")) / 100
 tip_amount = food_amount * tip_percentage
 
-print("$" + str(tip_amount))
+#print("$" + str(tip_amount))
+
+total = food_amount + tip_amount
+print("$" + str(total))
