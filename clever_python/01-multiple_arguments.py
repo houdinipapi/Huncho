5fac899ccc1a625badd55ee535911ce15a8fbb93
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-27 16:55:04 +0000

    multiple arguments

---
+++ clever_python/01-multiple_arguments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def greeting(my_greeting, my_name):
+    my_greeting = input("Say Hello in your language: ")
+    my_name = input("What's your name? ")
+    print(f"{my_greeting} {my_name}")
+
+
+greeting("my_greeting", "my_name")
