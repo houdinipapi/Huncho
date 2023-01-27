977f15e2f98f2cfcb75a0a107f4e447587e3303c
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-27 16:56:11 +0000

    multiple arguments

--- clever_python/01-multiple_arguments.py
+++ clever_python/01-multiple_arguments.py
@@ -4,7 +4,7 @@
 def greeting(my_greeting, my_name):
     my_greeting = input("Say Hello in your language: ")
     my_name = input("What's your name? ")
-    print(f"{my_greeting} {my_name}")
+    print(f"{my_greeting} {my_name}?")
 
 
 greeting("my_greeting", "my_name")
