854fd2e22a44496f5a495affa458d5f6e6ffaa98
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-27 14:39:39 +0000

    baby weather

--- clever_python/baby_weather_app.py
+++ clever_python/baby_weather_app.py
@@ -2,7 +2,7 @@
 
 weather = input("What's the weather?? ")
 
-if weather == "rain" | "RAIN":
+if weather == "rain" or "RAIN":
     print("Carry your umbrella")
 else:
     print("Wear your sunglasses")
