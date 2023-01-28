c65f7047bca10d7fa4fb6bd4678827e911415347
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-28 18:20:26 +0000

    tip function

---
+++ clever_python/Functions/name_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def name_function():
+    print("Houdini")
+    print("Papi")
+    print("Chulo")
+    print("HUncho")
+
+
+name_function()
---
+++ clever_python/Functions/tip_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def total_food(tip_percentage, food_amount):
+    food_amount = input("Enter food amount: ")
+    tip_percentage = input("Enter tip percentage: ")
+
+    tip_amount = tip_percentage / 100
+
+    total = food_amount + tip_amount
+    return total
+
+
+print("total")
