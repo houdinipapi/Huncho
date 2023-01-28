3d7199dd5c4da782d93dba2a94442842c164ad76
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-28 18:27:47 +0000

    tip function

--- clever_python/Functions/tip_calculator.py
+++ clever_python/Functions/tip_calculator.py
@@ -5,7 +5,7 @@ def total_food(tip_percentage, food_amount):
     food_amount = input("Enter food amount: ")
     tip_percentage = input("Enter tip percentage: ")
 
-    tip_amount = int(tip_percentage) / 100
+    tip_amount = int(food_amount) * (int(tip_percentage) / 100)
 
     total = int(food_amount) + tip_amount
     return total
