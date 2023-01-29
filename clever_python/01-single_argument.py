25157b6732c3a4f0eeeb15cf34cd86378668e713
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-29 21:36:44 +0000

    changes

--- clever_python/01-single_argument.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-def name(myname):
-    myname = input("What's your name? ")
-    print(myname)
-
-
-name("myname")
--- clever_python/multiple_arguments.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-def greeting(greet, name):
-    print(f"{greet} {name}!")
-
-
-greeting("Niaje", "Papi")
