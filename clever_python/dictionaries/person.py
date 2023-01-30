31bd21a84bfdd0dcaf94a322fef4b79a1321a970
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-30 15:19:31 +0000

    dictionaries in python

--- clever_python/dictionaries/person.py
+++ clever_python/dictionaries/person.py
@@ -6,4 +6,4 @@ person = {
         "phone": "Huawei"
         }
 print(person["phone"])
-print(f"My phone is a {person['phone']}.")
+print(f"Hi, My name is {person['name']},\nI am wearing a {person['shirt']} shirt,\nMy phone is a {person['phone']}")
