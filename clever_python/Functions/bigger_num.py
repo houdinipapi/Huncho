e40dedaad88a58ea4ed0533a80e82243029f8ebc
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-30 09:19:23 +0000

    bigger num

---
+++ clever_python/Functions/bigger_num.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def bigger_num(num1, num2):
+    """
+    Given two numbers, return the bigger one.
+    >>> bigger_num(3, 4)
+    4
+    """
+    num1 = input(int("Insert num1: "))
+    num2 = input(int("Insert num2: "))
