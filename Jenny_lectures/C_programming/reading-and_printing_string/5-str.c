35aaa8683b2094de29fc0c5585ffcbbccd5d6c01
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-02 20:25:06 +0000

    slicing

---
+++ Jenny_lectures/C_programming/reading-and_printing_string/5-str.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - reading and printing strings
+ */
+
+void main(void)
+{
+	char name[30];
+
+	printf("Enter your name: ");
+	gets(name);
+
+	printf("%.5s\n", name);
+	printf("%10s", &name[2]);
+}
