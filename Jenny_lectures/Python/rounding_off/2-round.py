646cfb3985fbec6f2cf897c5b35200fbd34bf8e1
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-03 06:10:39 +0000

    rounding off

--- Jenny_lectures/Python/rounding_off/2-round.py
+++ Jenny_lectures/Python/rounding_off/2-round.py
@@ -2,7 +2,6 @@
 
 num = input("Enter a number: ")
 round_off = input("Enter the rounding off value: ")
-int(num), int(round_off)
 
-new_num = round(num, round_off)
-print("The new number is str(new_num)")
+new_num = round(int(num), int(round_off))
+print("The new number is ,new_num")
