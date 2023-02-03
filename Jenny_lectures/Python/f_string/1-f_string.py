b1a9ac3b7acd88bfb87a88d5e8d9fbfb3b8f6838
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-03 06:17:33 +0000

    f_string

---
+++ Jenny_lectures/Python/f_string/1-f_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+name = input("What is your name? ")
+age = int(input("Enter your age: "))
+weight = float(input("Enter your weight: "))
+
+print(f"Hello {name}\nYou are {age} years old\nYou weigh {weight} kgs")
