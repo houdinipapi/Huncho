5979333e30a5964ee168e53e04edc10da0835ef4
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-12 09:18:30 +0000

    dictionaries

---
+++ clever_python/dictionaries/02-person.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+person = {
+        "first_name": "Houdini"
+        #"second_name": "Huncho"
+        "school": "Holberton School"
+        "course": "Software Engineering"
+        }
+
+print(person)
