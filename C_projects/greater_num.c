808f6c64edab181a6bd6b2786f9a65f65ce11d9d
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-14 22:46:46 +0000

    greater num

--- C_projects/greater_num.c
+++ C_projects/greater_num.c
@@ -6,4 +6,15 @@
 
 void main(void)
 {
+	int i, j, k;
 
+	printf("Enter three numbers: ");
+	scanf("%d %d %d", &i, &j, &k);
+
+	if (i > j && i > k)
+	{
+		printf("%d is the greatest number\n");
+	}
+	else if (j > i && j > k)
+	{
+		printf(
