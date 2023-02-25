f7bce7f63893dc97e73e83843fb6c81173476408
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-25 11:24:22 +0000

    string concatenation without predefined function

--- Jenny_lectures/string_concatenation/1-str_con.c
+++ Jenny_lectures/string_concatenation/1-str_con.c
@@ -9,12 +9,20 @@ void main(void)
 {
 	char first_name[30];
 	char second_name[15];
-	int l1;
+	int len1, len2, i;
 
 	printf("Enter your first and second name:\n");
 	scanf("%s %s", first_name, second_name);
 
-	l1 = strlen(first_name);
+	len1 = strlen(first_name);
+	len2 = strlen(second_name);
 
-	printf("%d\n", l1);
+	printf("%d\n", len1);
+	printf("%d\n", len2);
+
+	for (i = 0; i <= len2; i++)
+	{
+		first_name[len1 + i] = second_name[i];
+	}
+	printf("%s\n", first_name);
 }
