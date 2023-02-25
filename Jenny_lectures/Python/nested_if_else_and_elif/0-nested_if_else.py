581e52e4a8cb5baf45159a592964e726c22141c3
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-25 15:24:08 +0000

    Even and Odd

---
+++ Jenny_lectures/Python/nested_if_else_and_elif/0-nested_if_else.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+    if (num > 30):
+        print("Number is greater than 30!!")
+else:
+    print("Odd Number")
+
+print("Bye!!")
