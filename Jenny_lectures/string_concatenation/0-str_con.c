0b8ee5ca9a4e3776a804c23ea5b32376c83dbce7
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-25 11:03:05 +0000

    string concatenation

---
+++ Jenny_lectures/string_concatenation/0-str_con.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - concatenates string
+ */
+
+void main(void)
+{
+	char first_name[30];
+	char second_name[10];
+
+	strcat(first_name, second_name);
+
+	printf("%s\n", first_name);
+}
