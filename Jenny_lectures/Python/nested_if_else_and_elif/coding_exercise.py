7684d38d9375736103ee6bad44d17ad003dd02f0
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-25 16:00:24 +0000

    coding exercise

---
+++ Jenny_lectures/Python/nested_if_else_and_elif/coding_exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+weight = float(input("Enter your weight: "))
+height = float(input("Enter your height: "))
+
+bmi = round(weight/(height ** 2))
+
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi} and you are underweight.")
+elif bmi <= 25:
+    print(f"Your BMI is {bmi} and you have a normal weight.")
+elif bmi <= 30:
+    print(f"Your BMI is {bmi} and you are overweight.")
+elif bmi <= 35:
+    print(f"Your BMI is {bmi} and you are Obese.")
+else:
+    print("Your BMI is {bmi} and you are clinically obese.")
