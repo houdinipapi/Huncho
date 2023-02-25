5bc5f990adeb0bd036d1d07cdfb539983c7df541
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-25 11:06:41 +0000

    string concatenation

--- Jenny_lectures/string_concatenation/0-str_con.c
+++ Jenny_lectures/string_concatenation/0-str_con.c
@@ -10,6 +10,9 @@ void main(void)
 	char first_name[30];
 	char second_name[10];
 
+	printf("Enter your first and second name:\n");
+	scanf("%s %s", first_name, second_name);
+
 	strcat(first_name, second_name);
 
 	printf("%s\n", first_name);
