dad93f419eb24c74775972af1724d4e3cec82857
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-26 00:18:59 +0000

    pizza order

--- Jenny_lectures/Python/multiple_if_statements/pizza_order.py
+++ Jenny_lectures/Python/multiple_if_statements/pizza_order.py
@@ -9,25 +9,43 @@ if pizza_size == 'S' or pizza_size == 's':
     bill += 10
     print("Small Pizza price is $10")
 
+    add_pepperoni = input("Do you want pepperoni? (Y/N) ")
+    if add_pepperoni == 'Y' or add_pepperoni == 'y':
+        bill += 3
+
+    extra_cheese = input("Would you like extra cheese? (Y/N) ")
+    if extra_cheese == 'Y' or extra_cheese == 'y':
+        bill += 2
+
+    print(f"Your total bill is ${bill}")
+
 elif pizza_size == 'M' or pizza_size == 'm':
     bill += 15
     print("Medium Pizza price is $15")
+    
+    add_pepperoni = input("Do you want pepperoni? (Y/N) ")
+    if add_pepperoni == 'Y' or add_pepperoni == 'y':
+        bill += 5
+
+    extra_cheese = input("Would you like extra cheese? (Y/N) ")
+    if extra_cheese == 'Y' or extra_cheese == 'y':
+        bill += 2
+
+    print(f"Your total bill is ${bill}")
 
 elif pizza_size == 'L' or pizza_size == 'l':
     bill += 20
     print("Large Pizza price is $20")
 
-
-add_pepperoni = input("Do you want pepperoni? (Y/N) ")
-if add_pepperoni == 'Y' or add_pepperoni == 'y':
-    if pizza_size == 'S' or pizza_size == 's':
-        bill += 3
-
-    else:
+    add_pepperoni = input("Do you want pepperoni? (Y/N) ")
+    if add_pepperoni == 'Y' or add_pepperoni == 'y':
         bill += 5
 
-extra_cheese = input("Would you like extra cheese? (Y/N) ")
-if extra_cheese == 'Y' or extra_cheese == 'y':
-    bill += 2
+    extra_cheese = input("Would you like extra cheese? (Y/N) ")
+    if extra_cheese == 'Y' or extra_cheese == 'y':
+        bill += 2
 
-print(f"Your total bill is ${bill}")
+    print(f"Your total bill is ${bill}")
+
+else:
+    print("ENTER CORRECT INPUT!!!")
