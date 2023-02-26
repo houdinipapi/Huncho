9742263db47ab93a998cb68e2de4d29fa1562edc
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-26 09:52:26 +0000

    append list

---
+++ Jenny_lectures/Python/Lists/append/0-append_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''adding a name at the end of the list'''
+
+names = ["Houdini", "Papi", "Chulo", "Huncho"]
+
+names.append("Chingfos")
+
+print(names)
---
+++ Jenny_lectures/Python/Lists/append/1-append_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''adds a fruit at the end of the list'''
+
+fruits = ["Oranges", "Apples", "Mangoes", "Bananas"]
+print(fruits)
+
+fruits.append("Tangerines")
+
+print(fruits)
