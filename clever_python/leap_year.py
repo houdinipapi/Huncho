58c1a21a770d30b5f2480f3daca0f99f4eb349aa
houdinipapi <ochiengcliff.co@gmail.com>
2023-02-27 19:26:11 +0000

    leap year

--- clever_python/leap_year.py
+++ clever_python/leap_year.py
@@ -4,4 +4,8 @@ year = int(input("Enter a year: "))
 
 if year % 4 == 0:
     if year % 100 = 0:
-        
+        print("Leap Year")
+        if year % 400:
+            print("Not a Leap Year")
+        else:
+            print("Leap Year")
