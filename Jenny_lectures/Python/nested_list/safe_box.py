4da2ab4a1aa0d52fe9d45519e2c92dda9f23d591
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-05 07:15:02 +0000

    safe box

--- Jenny_lectures/Python/nested_list/safe_box.py
+++ Jenny_lectures/Python/nested_list/safe_box.py
@@ -11,5 +11,5 @@ print(f"{row1}\n{row2}\n{row3}")
 row_col_num = input("Pick a row and column number: ")
 print(row_col_num)
 
-row_num[0] = int(row_col_num)
-print(row_num[0])
+row_num = int(row_col_num[0])
+print(row_num)
