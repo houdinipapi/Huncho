2b78110e59b6bc4106cf9048b581a00d9b413918
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-05 07:13:28 +0000

    safe box

--- Jenny_lectures/Python/nested_list/safe_box.py
+++ Jenny_lectures/Python/nested_list/safe_box.py
@@ -8,5 +8,8 @@ row3 = [3, 3, 3]
 
 print(f"{row1}\n{row2}\n{row3}")
 
-user_num = input("Enter two numbers: ")
-print(user_num)
+row_col_num = input("Pick a row and column number: ")
+print(row_col_num)
+
+row_num[0] = int(row_col_num)
+print(row_num[0])
