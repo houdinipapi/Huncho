ecb50afc43ce67da1de2d14bba6c9625c854f249
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-05 07:28:51 +0000

    safe box

--- Jenny_lectures/Python/nested_list/safe_box.py
+++ Jenny_lectures/Python/nested_list/safe_box.py
@@ -6,6 +6,8 @@ row1 = [1, 1, 1]
 row2 = [2, 2, 2]
 row3 = [3, 3, 3]
 
+matrix = [row1, row2, row3]
+
 print(f"{row1}\n{row2}\n{row3}")
 
 row_col_num = input("Pick a row and column number: ")
@@ -15,3 +17,8 @@ row_num = int(row_col_num[0])
 col_num = int(row_col_num[1])
 print(f"Row: {row_num}")
 print(f"Column: {col_num}")
+
+row_selected = matrix[row_num - 1]
+row_selected[col_num - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
