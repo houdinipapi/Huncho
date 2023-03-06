b290f47f807a4558671a0ad9b019a18547bf0736
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-06 15:30:31 +0000

    string reversal

---
+++ Jenny_lectures/C_programming/string_reversal/2-string_reversal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - string reversal
+ */
+
+void main(void)
+{
+	int j, i, name_len;
+	char name[30], ch;
+
+	printf("Enter your name: ");
+	gets(name);
+
+	name_len = strlen(name);
+
+	for (i = 0, j = (name_len - 1); i > (name_len / 2); i++, j--)
+	{
+		ch = name[i];
+		name[i] = name[j];
+		name[j] = ch;
+	}
+	printf("%s\n", name);
+}
