5c53001241edc7b999945dd8f01108a114b5b533
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-18 12:51:18 +0000

    practice

--- clever_python/1-search_replace.py
+++ clever_python/1-search_replace.py
@@ -10,3 +10,12 @@ for i in range(0, n):
     my_list.append(element)
 
 print(my_list)
+
+for i in range(len(my_list)):
+
+    if my_list[i] == 2:
+        my_list[i] = 89
+
+    new_list = my_list.copy()
+
+print(new_list)
---
+++ clever_python/set_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+my_list = [1, 2, 3, 4]
+new_list = set(my_list)
+
+print(my_list)
+print(new_list)
