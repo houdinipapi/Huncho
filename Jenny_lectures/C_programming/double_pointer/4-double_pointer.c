6844742e212d05648fa290737266b89df4ea7661
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-18 21:01:49 +0000

    address

---
+++ Jenny_lectures/C_programming/double_pointer/4-double_pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - pointers
+ */
+
+void main(void)
+{
+	int a = 25;
+	int *p = &a;
+	int **q = &p;
+	int ***r = &q;
+
+	printf("a = %d\n", a);
+	printf("a = %d\n", *p);
+	printf("a = %d\n", **q);
+	printf("a = %d\n", ***r);
+
+	printf("Address of a = %x\t%x", &a, p);
+}
