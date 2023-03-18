06b16c7de938f4039877994144886f19dbc4c4cc
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-18 13:40:21 +0000

    Practice test

--- clever_python/3-common_elements.py
+++ clever_python/3-common_elements.py
@@ -2,10 +2,10 @@
 def common_elements(set_1, set_2):
     return (set_1 & set_2)
 
-
+'''
 set_1 = {"Fashion", "Design", "Fine_art"}
 set_2 = {"Fine_art", "Sculpture", "Drawing"}
 
 c_set = common_elements(set_1, set_2)
 
-print(sorted(list(c_set)))
+print(c_set)'''
