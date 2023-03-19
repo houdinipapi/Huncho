b4e50518aa0bf1cedbc4370c7b6e25d436f55a05
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-19 19:52:09 +0000

    pointer addition

---
+++ Jenny_lectures/C_programming/pointer_arithmetic/2-pointer_add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - addition of pointers
+ */
+
+void main(void)
+{
+	int a[7] = {1, -5, 6, 4, -3, 2, 9};
+	int *p = &a[0];
+
+	printf("Value is: %d\n", *p);
+
+	p = p + 3;
+	printf("Value is: %d\n", *p);
+}
