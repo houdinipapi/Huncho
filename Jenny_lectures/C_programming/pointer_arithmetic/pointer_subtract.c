e4dd3d646d8560022e55a3e83e2936c0469914f4
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-20 20:13:41 +0000

    subtratcting pointers

--- Jenny_lectures/C_programming/pointer_arithmetic/pointer_subtract.c
+++ Jenny_lectures/C_programming/pointer_arithmetic/pointer_subtract.c
@@ -7,8 +7,8 @@
 void main(void)
 {
 	int arr[] = { 3, 4, 6, -2, -5};
-	int *p = a;
-	int *q = &a[3];
+	int *p = arr;
+	int *q = &arr[3];
 
 	printf("q - p is %d\n", q - p);
 	printf("p - q is %d\n", p - q);
