f83c086e14d34358ff37aa04464c98f131987066
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-23 18:28:01 +0000

    rock paper scissor

--- Jenny_lectures/Python/nested_list/rock_paper_scissors.py
+++ Jenny_lectures/Python/nested_list/rock_paper_scissors.py
@@ -5,6 +5,7 @@ A rock, paper, and scissor game
 
 import random
 
-user_choice = int(input("Pick a number: (0 - Rock, 1 - Paper, 2 - Scissor)"))
-comp_choice = random.randomint(0, 2)
+user_choice = int(input("Pick a number: (0 - Rock, 1 - Paper, 2 - Scissor) "))
+comp_choice = random.randint(0, 2)
 print(comp_choice)
+print(user_choice)
