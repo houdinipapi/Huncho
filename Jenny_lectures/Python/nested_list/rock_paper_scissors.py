a87d534417e57a0620e01c9f7688001939151c02
houdinipapi <ochiengcliff.co@gmail.com>
2023-03-23 18:35:34 +0000

    rock paper scissor

--- Jenny_lectures/Python/nested_list/rock_paper_scissors.py
+++ Jenny_lectures/Python/nested_list/rock_paper_scissors.py
@@ -7,5 +7,5 @@ import random
 
 user_choice = int(input("Pick a number: (0 - Rock, 1 - Paper, 2 - Scissor) "))
 comp_choice = random.randint(0, 2)
-print(comp_choice)
-print(user_choice)
+print(f"The computer's choice is {comp_choice}")
+print(f"Your choice is {user_choice}")
