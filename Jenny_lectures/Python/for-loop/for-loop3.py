c451ee558facad76f1ec28a00a9a8f8c8db1274f
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-02 18:46:55 +0000

    traversing through a loop

--- Jenny_lectures/Python/for-loop/for-loop3.py
+++ Jenny_lectures/Python/for-loop/for-loop3.py
@@ -5,7 +5,10 @@ for loop - traversing through a sequence
 '''
 
 nums = [3, 5, 7, 9, -4]
+square_list = []
 
 for i in nums:
     square = i ** 2
     print(square)
+    square_list.append(square)
+print(square_list)
