03d5645284f9a5712796e8d3d93e31012c9ec68b
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-02 18:15:53 +0000

    superset

---
+++ Jenny_lectures/Python/sets/superset2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+issuperset - checks if set1 contains all the elements of set2
+'''
+
+set1 = {'Houdini', 'Papi', 'Chulo', 'John', 'Mike', 'Cliff'}
+set2 = {'John', 'Cliff', 'Mike'}
+
+print(set2.issuperset(set1))
