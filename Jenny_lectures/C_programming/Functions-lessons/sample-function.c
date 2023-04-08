67030cfa65cb0408ced8dc951ac4974ba9a163c3
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-08 20:13:35 +0000

    sample function

--- Jenny_lectures/C_programming/Functions-lessons/sample-function.c
+++ Jenny_lectures/C_programming/Functions-lessons/sample-function.c
@@ -9,16 +9,19 @@ void sum(int x, int y);
 
 void main(void)
 {
+	int total;
 	int x = 15;
 	int y = 20;
 
 	sum(x, y);
 
-	printf("Sum is %d\n", sum);
+	printf("Sum is %d\n", total);
 }
 
 void sum(int x, int y)
 {
-	sum = x + y;
+	int total;
+	total = x + y;
+
 	printf("x = %d\ny = %d\n", x, y);
 }
