4a19c8959d08f0bea5f600c849c0700ea683b8aa
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-09 09:51:49 +0000

    no arguments, no return type

---
+++ Jenny_lectures/C_programming/Functions-lessons/No Argument Without Return Type/0-sample_function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * sum() - function with no argument and no return type
+ * 		adds two numbers.
+ */
+
+void sum(void);
+
+void main(void)
+{
+	sum();
+}
+
+void sum(void)
+{
+	int a, b, sum = 0;
+	sum = a + b;
+
+	printf("sum = %d\n", sum);
+}
