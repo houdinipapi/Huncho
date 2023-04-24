2cc84439bfd62809fff8b548b0b3560d4254acde
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-24 23:57:14 +0300

    Baby Weather Function

--- clever_python/2-baby_weather_app.py
+++ clever_python/2-baby_weather_app.py
@@ -4,8 +4,14 @@
 def baby_weather():
     weather = int(input("Pick a number between 1 to 4: "))
     sun = "It is SUNNY!\nWear your sunglasses!"
+    rain = "It is RAINY!\nCarry an umbrella!"
+    fail = "ERROR"
 
     if weather == 1:
         return sun
+    elif weather == 2:
+        return rain
+    else:
+        return fail
 
 print(baby_weather())
