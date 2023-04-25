edcc948b4909d11a85117f94a70fec521c117ff3
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-25 16:23:25 +0300

    lambda function

---
+++ clever_python/lambda_function.py
@@ -0,0 +1,11 @@
+sum1 = lambda a, b: a + b
+
+#greet = lambda greet, name: f"{greet} {name}"
+
+#print(greet('Hello', 'Papi'))
+#print(sum1(3, 4))
+
+assert sum1(3, 5) == 8, 'Test 1 Passed'
+assert sum1(-20, 30) == 10, 'Test 2 Passed'
+
+print('All Tests Passed (2/2)')
