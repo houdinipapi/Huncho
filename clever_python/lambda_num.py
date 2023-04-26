5991507d6156eed2ed5a310274e13cc09f2a91b6
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-26 17:40:20 +0300

    double number using map function

---
+++ clever_python/lambda_num.py
@@ -0,0 +1,15 @@
+"""
+Doubling the nmbers in a list
+>>> list(map(lambda num: num * 2, [2, 4, 6]))
+[4, 8, 12]
+
+Squaring the numbers in list
+>>> list(map(lambda num: num ** 2, [2, 4, 6]))
+[4, 16, 36]
+"""
+
+
+print(list(map(lambda num: num * 2, [3, 6, 9])))
+
+
+print(list(map(lambda num: num ** 2, [3, 6, 9])))
