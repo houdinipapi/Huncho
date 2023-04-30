21f758356de30c77d29ce1f3d6d9cabbc965c3b7
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-30 21:04:14 +0000

    try_except_function.py

--- clever_python/basic_functions/try_except_function.py
+++ clever_python/basic_functions/try_except_function.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """
 A function that uses try() and except() keywords
+"""
+
+
+def try_and_except():
+    user_input = input("Enter a random input: ")
+    try:
+        new_user_input = int(user_input)
