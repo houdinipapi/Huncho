5d2b3b0fa1b50a82f4af9cab284c860c059b38db
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-04 13:17:06 +0300

    HTTP Requests

--- freeCodeCamp_Python-for-Everybody/main.py
+++ freeCodeCamp_Python-for-Everybody/main.py
@@ -1,5 +1,6 @@
 # --IMPORTED MODULES
 import re
+import socket
 
 # # This is a sample Python script.
 #
@@ -513,3 +514,18 @@ for line in f_hand:
     num = stuff[0]
     num_list.append(num)
 print('Maximum string:', max(num_list))  # --> Finding the maximum string
+
+# HTTP REQUEST IN PYTHON
+
+print("MAKING A HTTP REQUEST IN PYTHON:")
+my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_sock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+my_sock.send(cmd)
+
+while True:
+    data = my_sock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode())
+my_sock.close()
