d1f5a16b243a0510f32174038ed83fd7db486244
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-06 19:37:23 +0300

    DATABASES

--- freeCodeCamp_Python-for-Everybody/DATABASES/main-4.py
+++ freeCodeCamp_Python-for-Everybody/DATABASES/main-4.py
@@ -67,3 +67,19 @@ for entry in all_stuff:
         continue
 
     print(name, artist, album, count, rating, length)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) VALUES (?)''', (artist,))
+    cur.execute('SELECT id FROM Artist WHERE name = ?', (artist,))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
+        VALUES (?, ?)''', (album, artist_id))
+    cur.execute('SELECT id FROM Album WHERE title = ?', (album,))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, len, rating, count)
+        VALUES (?, ?, ?, ?, ?)''',
+                (name, album_id, length, rating, count))
+
+    conn.commit()
