a43b512f0de1c71bf7ecc8a419be6c2947eff315
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-08 23:04:02 +0300

    remove duplicates

--- LeetCode/remove_duplicates.py
+++ LeetCode/remove_duplicates.py
@@ -1,14 +1,19 @@
-def remove_duplicates(nums):
-    if len(nums) == 0:
-        return 0
+# def remove_duplicates(nums):
+#     if len(nums) == 0:
+#         return 0
+#
+#     k = 1
+#     for i in range(1, len(nums)):
+#         if nums[i] != nums[i - 1]:
+#             nums[k] = nums[i]
+#             k += 1
+#
+#     return k
+#
+#
+# print(remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 
-    k = 1
-    for i in range(1, len(nums)):
-        if nums[i] != nums[i - 1]:
-            nums[k] = nums[i]
-            k += 1
 
-    return k
+"""
 
-
-print(remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+"""
