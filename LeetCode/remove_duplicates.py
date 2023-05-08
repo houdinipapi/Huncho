3724bd966e6f4c507a5ab898dbe2c451515c42ad
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-08 23:13:00 +0300

    remove duplicates

--- LeetCode/remove_duplicates.py
+++ LeetCode/remove_duplicates.py
@@ -15,5 +15,18 @@
 
 
 """
+- Given an integer list [nums] sorted in ascending order,
+remove the duplicates in-place such that each unique element appears only once.
+- The relative order of the elements should be kept the same.
+- Then return the number of unique elements in [nums].
+- Consider the number of unique elements of [nums] to be k, to get accepted, you need to do the following things:
+    - Change the list [nums] such that the first k elements of [nums] contain the unique elements in the order
+    they were present in [nums] initially.
+    - The remaining elements of [nums] are not important as well as the size of [nums].
+    - Return k.
 
+>>> remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+5
 """
+
+def
