3c53ebf080336d367b0cde44ce17d2aecb4999cc
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-08 23:46:38 +0300

    remove elements

--- LeetCode/remove_element.py
+++ LeetCode/remove_element.py
@@ -10,10 +10,23 @@ Change the list [nums] such that the first k elements of [nums] contain the elem
 The remaining elements of [nums] are not important as well as the size of [nums].
 Return k.
 
->>> remove_element(nums = [3,2,2,3], val = 3)
+>>> remove_elements(nums = [3,2,2,3], val = 3)
 2
 """
 
 
 def remove_elements(nums: list, val: int) -> int:
-    # Initialize variable to keep track of number of number of elements
+    # Initialize variable to keep track of number of elements not equal to val
+    k = 0
+
+    # Loop through each element in the nums list
+    for i in range(len(nums)):
+        # If the element is not equal to val
+        if nums[i] != val:
+            # Replace the kth element with the ith element
+            nums[k] = nums[i]
+            # Increment k to keep track of number of elements not equal to val
+            k += 1
+
+    # Return k, which represents the number of elements not equal to val
+    return k
