e28dc49139aa60456a6d215d5abaa70b1cbc029b
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-09 09:49:40 +0300

    Email sender

---
+++ 20-Beginner_Python_projects/email_sender.py
@@ -0,0 +1,8 @@
+"""
+This program sends email to a user using Python
+"""
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
