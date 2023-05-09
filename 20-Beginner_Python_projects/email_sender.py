f8ae035cd284b6ab9bf4107f7997fb57b7c1ca29
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-09 10:37:04 +0300

    Email sender

--- 20-Beginner_Python_projects/email_sender.py
+++ 20-Beginner_Python_projects/email_sender.py
@@ -6,4 +6,20 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
+from app_code import pass_code
 
+# Set up email sender information
+email_sender = "ochiengcliff.co@gmail.com"
+email_password = pass_code
+email_recipient = "taved91664@larland.com"
+
+# Set up message information
+subject = "Test Email"
+body = """
+This a is a test email.
+Let's see what it can do.
+"""
+attachment_file = "sample.pdf"
+image_file = "example.png"
+
+# Set up message
