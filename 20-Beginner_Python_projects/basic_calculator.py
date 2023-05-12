af4dbf4eafccfa818859d09c1cfbb95bfc67a6cf
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-12 22:20:58 +0300

    defining functions of a basic calculator

--- 20-Beginner_Python_projects/basic_calculator.py
+++ 20-Beginner_Python_projects/basic_calculator.py
@@ -7,8 +7,16 @@ Addition, subtraction, division, and multiplication until the user quits.
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
     return a - b
 
+
 def mul(a, b):
     return a * b
+
+
+def div(a, b):
+    return a / b
+
+
