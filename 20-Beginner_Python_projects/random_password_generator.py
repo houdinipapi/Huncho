97bb070832e5801093cf34a17e04541473b3c9df
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-13 11:01:10 +0300

    random password generator

--- 20-Beginner_Python_projects/random_password_generator.py
+++ 20-Beginner_Python_projects/random_password_generator.py
@@ -2,24 +2,36 @@
 Random password generator.
 """
 import random
+import string
 
-char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_+=-[]{};':<>,.?/"
+# --- PART 1 --- #
+# char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_+=-[]{};':<>,.?/"
+#
+# while True:
+#     password_len = int(input("Password length: "))
+#
+#     if password_len == 0:
+#         quit()
+#
+#     else:
+#         password_count = int(input("How many passwords: "))
+#         for paswd in range(0, password_count):
+#             password = ""
+#
+#             for character in range(0, password_len):
+#                 password_char = random.choice(char)
+#                 password += password_char
+#
+#             print(f"Password: {password}")
+#
+#         print("\n")
 
-while True:
-    password_len = int(input("Password length: "))
 
-    if password_len == 0:
-        quit()
+# --- PART 2 --- #
+def generate_password(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    new_password = ''.join(random.choice(characters) for _ in range(length))
+    return new_password
 
-    else:
-        password_count = int(input("How many passwords: "))
-        for paswd in range(0, password_count):
-            password = ""
 
-            for character in range(0, password_len):
-                password_char = random.choice(char)
-                password += password_char
-
-            print(f"Password: {password}")
-
-        print("\n")
+print(f"New Password: {generate_password()}")
