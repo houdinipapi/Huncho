f4d4a5042653d13a0c90f4f2b03eb680a3a02e3c
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-23 23:45:36 +0300

    deep thoughts

--- clever_python/CS50X-Python_Practicals/deep.py
+++ clever_python/CS50X-Python_Practicals/deep.py
@@ -4,12 +4,15 @@ the Universe and Everything, outputting Yes if the user inputs 42 or (case-insen
 Otherwise, output No.
 """
 
-answer = input("What is the answer to the Great Question of Life, the Universe, and Everything? ")
-if answer == 42:
-    print("Yes")
-elif answer == "forty-two" or "Forty-Two":
-    print("Yes")
-elif answer == "forty two" or "Forty Two":
-    print("Yes")
-else:
-    print("No")
+
+def deep_program():
+    answer = input("What is the answer to the Great Question of Life, the Universe, and Everything? ")
+    answer = answer.strip().lower().replace(" ", "")  # Removing leading/trailing spaces, converting to lowercase, and removing spaces inside
+
+    if answer == "42" or answer == "fortytwo":
+        print("Yes")
+    else:
+        print("No")
+
+
+deep_program()
