b797f2bade52372f5e3ced6beb2f24ecf68042ab
houdinipapi <ochiengcliff.co@gmail.com>
2023-05-23 17:47:56 +0000

    Trial and Error

--- clever_python/main.py
+++ clever_python/main.py
@@ -3,7 +3,7 @@ user_input = input()
 user_input = user_input.replace(" ", "***")
 print(user_input)
 
-name_list = ["Papi", "Chulo", "Houdini"]:
+name_list = ["Papi", "Chulo", "Houdini"]
     for i in name_list:
         if i == "Chulo":
             i.replace("Chulo", "Huncho")
