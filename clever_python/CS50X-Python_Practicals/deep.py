65c33c4c4d52e48c62221ebf5410f1f8a0342bb6
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-23 23:47:42 +0300

    deep thoughts

--- clever_python/CS50X-Python_Practicals/deep.py
+++ clever_python/CS50X-Python_Practicals/deep.py
@@ -9,7 +9,7 @@ def deep_program():
     answer = input("What is the answer to the Great Question of Life, the Universe, and Everything? ")
     answer = answer.strip().lower().replace(" ", "")  # Removing leading/trailing spaces, converting to lowercase, and removing spaces inside
 
-    if answer == "42" or answer == "fortytwo":
+    if answer == "42" or answer == "fortytwo" or "forty-two":
         print("Yes")
     else:
         print("No")
