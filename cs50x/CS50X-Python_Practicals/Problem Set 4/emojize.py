7117853bb779b687424d4743ee1cf114cdc08107
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-28 22:04:56 +0300

    Emojize

--- cs50x/CS50X-Python_Practicals/Problem Set 4/emojize.py
+++ cs50x/CS50X-Python_Practicals/Problem Set 4/emojize.py
@@ -3,3 +3,8 @@
 and then outputs the “emojized” version of that str,
 converting any codes (or aliases) therein to their corresponding emoji.
 """
+
+import emoji
+
+user_emoji = input("Input: ")
+print(f"Output: {emoji.emojize(user_emoji)}")
