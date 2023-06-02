c3b61f8590385844016a1ec19e2488d56d8beef8
houdinipapi <ochiengcliff.co@gmail.com>
2023-06-02 08:08:46 +0000

    NUMB3RS

--- cs50x/CS50P-Python_Practicals/Problem Set 7/numb3rs/numb3rs.py
+++ cs50x/CS50P-Python_Practicals/Problem Set 7/numb3rs/numb3rs.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
-
+"""
+- Implement a function called validate that expects an IPv4 address as input
+as a str and then returns True or False, respectively,
+if that input is a valid IPv4 address or not.
 """
 
-"""
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ").strip()))
+
+
+def validate(ip):
+    # Checking for matches
+    matches = r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+
+    # Checking if the matches are present in the input
+    if re.match(matches, ip) is not None:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
