4f2f9f87fb47520e8c9179fcf736554fb8aa8038
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-06 23:52:32 +0300

    scraping, generating, and saving the quotes in a PDF

--- 20-Beginner_Python_projects/Random Quote Generator/zen_quotes.py
+++ 20-Beginner_Python_projects/Random Quote Generator/zen_quotes.py
@@ -3,8 +3,13 @@
 - Generates the quotes randomly.
 """
 
+import os
 import requests
 import random
+from fpdf import FPDF, XPos, YPos
+
+# Define the path and filename for the PDF file
+pdf_path = os.path.join(r"C:\Users\ADMIN\PycharmProjects\firstProject\Huncho\20-Beginner_Python_projects\Random Quote Generator", "quotes.pdf")
 
 
 def random_quote():
@@ -24,4 +29,49 @@ def random_quote():
     return formatted_quote
 
 
-print(random_quote())
+# Saving the quotes in a PDF file
+def save_quotes_to_pdf(quotes):
+    pdf = FPDF()
+
+    # Set up the PDF document
+    pdf.set_font('helvetica', size=12)
+    pdf.add_page()
+
+    # Clear existing content
+    pdf.set_xy(0, 0)
+    pdf._out('')
+
+    # Write each quote to the PDF
+    for quote in quotes:
+        pdf.cell(0, 10, txt=quote, new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+
+    # Save the PDF file
+    pdf.output("quotes.pdf")
+
+
+# Load existing quotes from the PDF file
+existing_quotes = []
+try:
+    with open(pdf_path, "rb") as file:
+        pdf = FPDF()
+        pdf.set_auto_page_break(auto=True, margin=15)
+        pdf.add_page()
+        existing_quotes = file.read().decode("latin-1").splitlines()
+except FileNotFoundError:
+    pass
+
+# Generate new quotes
+new_quotes = []
+for _ in range(3):  # --> Generate 3 new quotes
+    quote = random_quote()
+    new_quotes.append(quote)
+
+# Combine existing quotes and new quotes
+combined_quotes = existing_quotes + new_quotes
+
+# Save all quotes (existing and new) to the PDF file
+save_quotes_to_pdf(combined_quotes)
+
+# Print the new_quotes
+for quote in new_quotes:
+    print(quote)
