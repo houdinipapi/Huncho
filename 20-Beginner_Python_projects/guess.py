604a285022d7a7d7185be5b13dbda36a973d79cf
houdinipapi <ochiengcliff.co@gmail.com>
2023-06-09 21:28:30 +0000

    Guess a number

--- 20-Beginner_Python_projects/guess.py
+++ 20-Beginner_Python_projects/guess.py
@@ -6,8 +6,9 @@
 import random
 import sys
 
-
+num_list = [1, 2, 3, 4, 5]
 try:
-    user_num = int(input("Enter a Number: ")
+    user_num = int(input("Enter a Number: "))
+    comp_num = random.choice(num_list)
 except ValueError:
     sys.exit("Not a Number")
