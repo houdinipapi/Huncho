093798dd0978fade245d97580751c62e34ec6208
houdinipapi <ochiengcliff.co@gmail.com>
2023-06-10 18:46:54 +0000

    Submitting the number on the command line

--- 20-Beginner_Python_projects/guess.py
+++ 20-Beginner_Python_projects/guess.py
@@ -8,14 +8,13 @@ import sys
 
 num_list = [1, 2, 3, 4, 5]
 comp_num = random.choice(num_list)
+user_num = int(sys.argv[1])
 
 while True:
     if len(sys.argv) < 2:
         sys.exit("Few Commands")
     else:
         try:
-            user_num = sys.argv[1]
-
             if user_num == comp_num:
                 sys.exit("CORRECT MATCH")
             else:
