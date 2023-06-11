cb318458e25965a737059c9a713ef1060ddb338c
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-11 15:24:24 +0300

    Guess a number

--- 20-Beginner_Python_projects/guess.py
+++ 20-Beginner_Python_projects/guess.py
@@ -8,16 +8,17 @@ import sys
 
 num_list = [1, 2, 3, 4, 5]
 comp_num = random.choice(num_list)
-user_num = int(sys.argv[1])
 
 while True:
-    if len(sys.argv) < 2:
-        sys.exit("Few Commands")
-    else:
-        try:
-            if user_num == comp_num:
-                sys.exit("CORRECT MATCH")
-            else:
-                continue
-        except ValueError:
-            sys.exit("Not a Number")
+    user_num = int(input("Guess a number: "))
+    try:
+        if user_num == comp_num:
+            print("CORRECT MATCH")
+            sys.exit(0)
+        else:
+            print("Try Again")
+            continue
+                
+    except ValueError:
+        print("Invalid Input")
+        sys.exit(1)
