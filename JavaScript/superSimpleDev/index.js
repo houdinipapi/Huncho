242587800fdfb2f26633f013b2a84cbbcc8166fc
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-14 13:38:48 +0300

    Numbers in JavaScript

---
+++ JavaScript/superSimpleDev/index.js
@@ -0,0 +1,23 @@
+// Numbers & Arithmetics
+// 1 Soup @ $10
+// 3 Burgers @ $8
+// 1 Ice cream @ $5
+// Calculate total cost
+
+let soup = 10;
+let burgers = 8;
+let iceCream = 5;
+
+total = soup + (burgers * 3) + iceCream;
+
+console.log("Total cost: $" + total)
+
+// 1 toaster @ $18.50
+// 2 shirts @ $7.50
+// Calculate total
+let toaster = 18.50
+let shirt = 7.50
+
+totalCost = toaster + (shirt * 2)
+
+console.log("Total cost: $" + totalCost)
