e6628f3b3eb41d8a513f0f5633f252ef44c7d44a
houdinipapi <ochiengcliff.co@gmail.com>
2023-06-18 21:47:06 +0000

    Create a database that does not exist

---
+++ MySQL/SQL Introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database hbtn_0c_0 in MySQL server.
+-- If the database hbtn_0c_0 already exists, the script should fail.
+-- Not allowed to use the SELECT or SHOW statements
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
