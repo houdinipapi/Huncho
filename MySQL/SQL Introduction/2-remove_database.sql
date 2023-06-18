ec646b92d5cb953bf8faee654fef7a65068f47bf
houdinipapi <ochiengcliff.co@gmail.com>
2023-06-18 21:52:33 +0000

    Deleting a database which exists

---
+++ MySQL/SQL Introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- A script that deletes the database hbtn_0c_0 in MySQL server.
+-- If the database hbtn_0c_0 doesn't exist, the script should fail.
+-- Not allowed to use the SELECT or SHOW statements.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
