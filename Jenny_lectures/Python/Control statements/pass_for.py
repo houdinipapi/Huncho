71c839481b104337957c9169f1a6f3c54e1a0a1f
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-29 15:19:38 +0300

    pass --> skips 7

--- Jenny_lectures/Python/Control statements/pass_for.py
+++ Jenny_lectures/Python/Control statements/pass_for.py
@@ -1,2 +1,11 @@
 for i in range(1, 11):
     pass  #Might use it in future --> An empty loop at the moment
+
+for i in range(1, 11):
+    if i == 7:
+        pass
+    else:
+        print(i)
+        print("Hello World!")
+
+print("WE OUTSIDE!!!")
