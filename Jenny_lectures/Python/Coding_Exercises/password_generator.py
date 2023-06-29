19f0bed41f6c9f7245538526a05f6fcebea2bfd8
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-29 13:10:48 +0300

    password generator ----> Adding letters

--- Jenny_lectures/Python/Coding_Exercises/password_generator.py
+++ Jenny_lectures/Python/Coding_Exercises/password_generator.py
@@ -1,1 +1,19 @@
 # A script that generates password randomly.
+import random
+import string
+
+p_letters = string.ascii_letters
+p_digits = string.digits
+p_punc = string.punctuation
+
+n_letters = int(input("No. of letters: "))
+n_digits = int(input("No. of digits: "))
+n_punc = int(input("Punctuations: "))
+
+password = ""
+
+for _ in range(1, (n_letters + 1)):
+    char = random.choice(p_letters)
+    password += char
+
+print(f"Password: {password}")
