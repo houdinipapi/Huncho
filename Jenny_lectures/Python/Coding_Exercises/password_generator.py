7e174d3c44d3ec138f242cec22fc7a40b35327ff
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-29 13:32:02 +0300

    Password generator

--- Jenny_lectures/Python/Coding_Exercises/password_generator.py
+++ Jenny_lectures/Python/Coding_Exercises/password_generator.py
@@ -12,12 +12,31 @@ n_punc = int(input("Punctuations: "))
 
 password = ""
 
+# Getting letters
 for _ in range(1, (n_letters + 1)):
     char = random.choice(p_letters)
     password += char
 
+# Getting digits/numbers
 for _ in range(1, n_digits + 1):
     char = random.choice(p_digits)
     password += char
 
-print(f"Password: {password}")
+# Getting special characters
+for _ in range(1, n_punc + 1):
+    char = random.choice(p_punc)
+    password += char
+
+# Converting str into a list
+password_list = list(password)
+print(password_list)
+
+# Shuffling the list
+random.shuffle(password_list)
+
+print(password_list)
+
+# Converting the list into a string
+pass_str = ""
+new_pass = pass_str.join(password_list)
+print(new_pass)
