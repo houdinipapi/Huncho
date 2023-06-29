651034a4d86c7ac8fd3e713ef935082e51b84ccd
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-29 14:10:18 +0300

    Exiting the loop using a sentinel value

--- Jenny_lectures/Python/while loop/while_loop.py
+++ Jenny_lectures/Python/while loop/while_loop.py
@@ -1,6 +1,23 @@
 # Simple while loop function
 count = 1
 
-while count <= 5: print(count); count += 1
+# while count <= 5: print(count); count += 1
+while count <= 5:
+    print(count)
+    count += 1
 
+    # Terminating the loop forcefully
+    if count == 3:
+        break
+else:
+    print("In ELSE block")
 print("Out of loop")
+
+# Exiting a while loop using a sentinel value i.e., special value
+number = int(input("Enter a number: (-1 to quit)"))
+while number != -1:
+    print(number)
+    number = int(input("Enter a number: (-1 to quit)"))
+else:
+    print("ELSE")
+print("WE OUTSIDE!!!")
