aa3e8b1a872a1183ac9ecdd49e217c3e74b02c31
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-30 17:15:08 +0300

    Adding two numbers which are passed as parameters

--- Jenny_lectures/Python/FUNCTIONS/sample_function.py
+++ Jenny_lectures/Python/FUNCTIONS/sample_function.py
@@ -6,10 +6,15 @@ def greet():
 greet()
 
 # With an argument
-
 name = input("Name: ")
 
 def new_greet(name):
     print(f"Hi, {name}")
 
 new_greet(name)
+
+# Sum function
+def add_num(a, b):
+    return a + b
+
+print(add_num(5, 7))
