8a6487302e6a1352e38fdb8b0d47d66f2eb4a422
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-30 17:38:08 +0300

    Types of arguments that can be passed to a function

---
+++ Jenny_lectures/Python/FUNCTIONS/argument_types.py
@@ -0,0 +1,26 @@
+# Types of arguments
+# Default
+# Positional
+# Keyword
+# Arbitrary :-
+#     - Arbitrary positional
+#     - Arbitrary keyword
+
+# Positional
+def greet(name, dept):
+    print(f"Hello, {name}.\nWelcome to {dept} department.")
+
+greet("Papi", "Fashion & Design")
+
+# Keyword Arguments
+greet(name="Houdini", dept="Engineering")
+
+# Positional & Keyword
+greet("Chulo", dept="Medicine")  # Keyword argument should be after positional argument to avoid syntax error.
+
+# Default Argument
+def new_greeting(name, dept, subject="Python"):
+    print(f"Hello {name}.\nThis is {dept} department.\nWe teach {subject}.")
+
+new_greeting("Houdini", "Applied Science")  # Will take default subject.
+new_greeting("Cliff", "Software", "JavaScript")  # Javascript will overwrite the default subject.
