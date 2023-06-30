43850dd1e3bf4a88f32dd40abbe617bb0f0bc26a
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-30 18:15:07 +0300

    Arbitrary arguments

--- Jenny_lectures/Python/FUNCTIONS/argument_types.py
+++ Jenny_lectures/Python/FUNCTIONS/argument_types.py
@@ -24,3 +24,13 @@ def new_greeting(name, dept, subject="Python"):
 
 new_greeting("Houdini", "Applied Science")  # Will take default subject.
 new_greeting("Cliff", "Software", "JavaScript")  # Javascript will overwrite the default subject.
+
+# Arbitrary Arguments  --> Can take any amount of variables
+def addition(*numbers):
+    total_sum = 0
+    for i in numbers:
+        total_sum += i
+    print(f"Sum: {total_sum}")
+
+addition(5, 6, 7, 8)
+addition(2, 3, 4, 5, 6, 7, 8, 9)
