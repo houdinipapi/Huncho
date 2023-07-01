61d8d961330c4cb237ad4778cadc53b57e022304
Houdini Papi <ochiengcliff.co@gmail.com>
2023-07-01 07:12:39 +0300

    number of paint cans required

--- Jenny_lectures/Python/FUNCTIONS/coding_exercise/paint_area_calculation.py
+++ Jenny_lectures/Python/FUNCTIONS/coding_exercise/paint_area_calculation.py
@@ -1,9 +1,13 @@
 import math
 
+
 def paint_calculation(height, width, coverage):
     area = height * width
-    no_of_cans = math.ceil(area / coverage)  # ceil() --> rounding off to the next number
+    no_of_cans = math.ceil(
+        area / coverage
+    )  # ceil() --> rounding off to the next number
     return f"Cans needed: {no_of_cans}"
+
 
 wall_height = int(input("Height: "))
 wall_width = int(input("Width: "))
