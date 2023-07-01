a9717724bea3d788e6aa90d03d47484a9c4ade5e
Houdini Papi <ochiengcliff.co@gmail.com>
2023-07-01 07:06:35 +0300

    function that calculates number of cans required to paint a wall

---
+++ Jenny_lectures/Python/FUNCTIONS/coding_exercise/paint_area_calculation.py
@@ -0,0 +1,12 @@
+import math
+
+def paint_calculation(height, width, coverage):
+    area = height * width
+    no_of_cans = math.ceil(area / coverage)  # ceil() --> rounding off to the next number
+    return f"Cans needed: {no_of_cans}"
+
+wall_height = int(input("Height: "))
+wall_width = int(input("Width: "))
+wall_coverage = 7
+
+print(paint_calculation(height=wall_height, width=wall_width, coverage=wall_coverage))
