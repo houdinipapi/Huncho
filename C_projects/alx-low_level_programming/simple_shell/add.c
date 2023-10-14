2b0f85876f6571095bcd54007f3ba31a7a8a6133
papihoudini <ochiengcliff.co@gmail.com>
2023-10-14 06:46:15 -0400

    Simple C programs

---
+++ C_projects/alx-low_level_programming/simple_shell/add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int a = 6;
+	int b = 9;
+	int sum = a + b;
+
+	printf("Sum is %d\n", sum);
+
+	return 0;
+}
---
+++ C_projects/alx-low_level_programming/simple_shell/subtract.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int a = 9;
+	int b = 3;
+	int diff = a - b;
+
+	printf("Difference is %d\n", diff);
+
+	return 0;
+}
